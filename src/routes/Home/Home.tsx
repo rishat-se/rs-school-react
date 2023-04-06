43b414f4e89462fe055db3c78f2d9b33f11830e7
Rishat Agzamov <rishat.se@gmail.com>
2023-04-06 17:36:38 -0400

    feat: move searchValue and fetching to Home component

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CardList from '../../components/CardList/CardList';
 import SearchBar from '../../components/SearchBar/SearchBar';
+import { API_URL } from '../../constants/constants';
+import { CardData } from '../../types/CardData';
 
 function Home() {
+  const [searchValue, setSearchValue] = useState(localStorage.getItem('searchValue') || '');
+  const [cards, setCards] = useState<CardData[]>([]);
+
+  useEffect(() => {
+    console.log('useEffect searchValue is ', searchValue);
+    fetch(`${API_URL}/?name=${searchValue}`)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then(({ info, results }) => setCards(results));
+  }, [searchValue]);
+
+  function handleSearchValueSubmit(searchValue: string) {
+    console.log('handleSubmit called');
+    setSearchValue(searchValue);
+    localStorage.setItem('searchValue', searchValue);
+  }
+
+  console.log('home render & searchValue is: ', searchValue);
   return (
     <div>
-      <SearchBar />
-      <CardList />
+      <SearchBar searchValue={searchValue} onSearchValueSubmit={handleSearchValueSubmit} />
+      <CardList cards={cards} />
     </div>
   );
 }
