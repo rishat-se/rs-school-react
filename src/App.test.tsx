13abdcdf6cc733e404cef24070e714387c3be2fb
Rishat Agzamov <rishat.se@gmail.com>
2023-05-02 04:06:17 -0400

    fix: fix unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,14 +4,17 @@ import { describe, it } from 'vitest';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { StaticRouter } from 'react-router-dom/server';
 
 describe('App', () => {
   it('test of presence of logo', () => {
     render(
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <StaticRouter location="">
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </StaticRouter>
     );
-    expect(screen.getByText(/RS-School React App/i)).toBeInTheDocument();
+    expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument();
   });
 });
